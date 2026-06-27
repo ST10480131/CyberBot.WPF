7ad7f01f6392ce14c36f555f7cd9988b0bef8cfb
achumile somdaka <ST10480131@rcconnect.edu.za>
2026-06-27 10:58:22 -0800

    Refactor Quiz class to improve question structure

--- Quiz.cs
+++ Quiz.cs
@@ -11,14 +11,14 @@ namespace CyberBot
 
         public List<Question> Questions = new List<Question>()
         {
-            // ================= TRUE / FALSE =================
+            
             new Question("Phishing is a cyber attack.", new string[] { "True", "False" }, 0),
             new Question("You should share your password with friends.", new string[] { "True", "False" }, 1),
             new Question("2FA improves account security.", new string[] { "True", "False" }, 0),
             new Question("Malware is harmless software.", new string[] { "True", "False" }, 1),
             new Question("Public WiFi is always safe.", new string[] { "True", "False" }, 1),
 
-            // ================= MULTIPLE CHOICE =================
+            
             new Question("What is the strongest password?",
                 new string[] { "123456", "password", "P@ssw0rd!2026", "admin123" }, 2),
 
@@ -35,7 +35,7 @@ namespace CyberBot
                 new string[] { "Download unknown files", "Click random links", "Verify websites", "Share OTP codes" }, 2)
         };
 
-     
+        
         public Question GetQuestion()
         {
             if (index < Questions.Count)
@@ -44,7 +44,7 @@ namespace CyberBot
             return null;
         }
 
-        
+       
         public bool Answer(int selectedIndex)
         {
             bool correct = Questions[index].CorrectIndex == selectedIndex;
@@ -57,19 +57,19 @@ namespace CyberBot
             return correct;
         }
 
-        //  SCORE 
+      
         public int GetScore()
         {
             return score;
         }
 
-        // FINISH CHECK 
+       
         public bool IsFinished()
         {
             return index >= Questions.Count;
         }
 
-        // RESET
+      
         public void Reset()
         {
             score = 0;
@@ -77,7 +77,7 @@ namespace CyberBot
         }
     }
 
-    // QUESTION CLASS 
+    
     public class Question
     {
         public string Text { get; set; }
