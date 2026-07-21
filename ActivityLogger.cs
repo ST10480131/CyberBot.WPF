4006363705c88bafeb360a4c65b64925362d8321
achumile somdaka <ST10480131@rcconnect.edu.za>
2026-07-21 11:02:15 -0800

    Refactor ActivityLogger to use database logging
    Refactored ActivityLogger to use a database for logging activities instead of file-based logging. Updated methods for logging and retrieving activities.

--- ActivityLogger.cs
+++ ActivityLogger.cs
@@ -1,39 +1,73 @@
 using System;
-using System.Collections.ObjectModel;
-using System.IO;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
-namespace CyberBot;
-
-public class ActivityLogger
+namespace CyberBot
 {
-    public static ActivityLogger Instance { get; } = new();
-    public ObservableCollection<string> Entries { get; } = new();
-
-    private readonly string Logfile = Path.Combine(AppContext.BaseDirectory, "activity.log");
-
-    private ActivityLogger()
+    public class ActivityLogger
     {
-        if (File.Exists(Logfile))
+        private static ActivityLogger instance;
+
+        public static ActivityLogger Instance
         {
-            foreach (var line in File.ReadAllLines(Logfile))
+            get
             {
-                Entries.Add(line);
+                if (instance == null)
+                    instance = new ActivityLogger();
+
+                return instance;
+            }
+        }
+
+        private Database db = new Database();
+
+        public List<string> Entries { get; } = new List<string>();
+
+        public void Log(string description)
+        {
+            Entries.Add(description);
+
+            using (SqlConnection con = db.GetConnection())
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO ActivityLog(ActivityType,Description) VALUES(@type,@desc)",
+                    con);
+
+                cmd.Parameters.AddWithValue("@type", "Chatbot");
+                cmd.Parameters.AddWithValue("@desc", description);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<string> GetLastActivities(int count = 10)
+        {
+            List<string> logs = new List<string>();
+
+            using (SqlConnection con = db.GetConnection())
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    @"SELECT TOP (@count) Description, ActivityDate
+                      FROM ActivityLog
+                      ORDER BY ActivityDate DESC",
+                    con);
+
+                cmd.Parameters.AddWithValue("@count", count);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    logs.Add(
+                        $"{Convert.ToDateTime(reader["ActivityDate"]):g} - {reader["Description"]}");
+                }
             }
 
+            return logs;
         }
     }
-
-    public void Log(string action)
-    {
-        var line = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {action}";
-        Entries.Add(line);
-        try { File.AppendAllText(Logfile, line + Environment.NewLine); } catch { }
-    }
-
-    public void Clear()
-    {
-        Entries.Clear();
-        try { File.Delete(Logfile); } catch { }
-    }
-
 }
